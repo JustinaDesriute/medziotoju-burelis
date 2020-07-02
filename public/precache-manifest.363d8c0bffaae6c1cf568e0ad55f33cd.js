6daaa374cbd7298c7a5517753e1c19ffecfcfe01
Justina Desriute <justinadesriute@Justinas-MBP.fritz.box>
2020-07-02 13:49:31 +0200

    add: build files for deployment

---
+++ public/precache-manifest.363d8c0bffaae6c1cf568e0ad55f33cd.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "82854e0a76955bf17c5b",
+    "url": "/static/css/main.0e0947d6.chunk.css"
+  },
+  {
+    "revision": "82854e0a76955bf17c5b",
+    "url": "/static/js/main.82854e0a.chunk.js"
+  },
+  {
+    "revision": "3379f378220ad3602bbe",
+    "url": "/static/css/1.875fc7c4.chunk.css"
+  },
+  {
+    "revision": "3379f378220ad3602bbe",
+    "url": "/static/js/1.3379f378.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "94b6c18db4e6415bf56931e4df5612b7",
+    "url": "/static/media/nariai.94b6c18d.jpg"
+  },
+  {
+    "revision": "860ad6b16bf8c0215a0a023d929ca36b",
+    "url": "/static/media/ribu_zemelapis.860ad6b1.png"
+  },
+  {
+    "revision": "f93d86cb2f7f8558925a03f18ebd1ad0",
+    "url": "/static/media/tryspirmininkai.f93d86cb.jpg"
+  },
+  {
+    "revision": "ed39b39340766032e5821be10e0a5606",
+    "url": "/static/media/meisteris.ed39b393.jpg"
+  },
+  {
+    "revision": "6d0a201967e44254793f45396fc641fc",
+    "url": "/static/media/meska.6d0a2019.jpg"
+  },
+  {
+    "revision": "e28d9433f1f879f4f696fe4f21c761b8",
+    "url": "/static/media/logo.e28d9433.jpg"
+  },
+  {
+    "revision": "9fb97e5df64bc2fa95e1fc7e408f8da7",
+    "url": "/static/media/camouflage.9fb97e5d.jpg"
+  },
+  {
+    "revision": "4ad954a474bf07c119d2b54160dbac21",
+    "url": "/index.html"
+  }
+];
